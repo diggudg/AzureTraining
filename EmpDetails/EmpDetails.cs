c1f1a4eeabafc83c609fe7183ccc1ad432dc0b39
Dotlogistic Infotech <ifw.pat@outlook.com>
2017-08-12 11:39:49 +0000

    aded worker role

---
+++ EmpDetails/EmpDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpDetails
+{
+    public class EmpDetailsInfo
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string ContactNo { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ WebRole1/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using EmpDetails;
+using Microsoft.Azure;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebRole1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            EmpDetailsInfo emp = new EmpDetailsInfo();
+            return View(emp);
+        }
+
+        [HttpPost]
+        public ActionResult Create(EmpDetailsInfo emp)
+        {
+            const string QueueName = "empQueue";
+            string connectionString = CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+            var nameSpaceManager = NamespaceManager.CreateFromConnectionString(connectionString);
+            if (!nameSpaceManager.QueueExists(QueueName))
+            {
+                nameSpaceManager.CreateQueue(QueueName);
+            }
+
+            MessagingFactory factory = MessagingFactory.CreateFromConnectionString(connectionString);
+            QueueClient myQ = factory.CreateQueueClient(QueueName);
+
+            BrokeredMessage bm = new BrokeredMessage(emp);
+
+            myQ.Send(bm);
+
+            ViewBag.Status = "Message Send";
+
+
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
